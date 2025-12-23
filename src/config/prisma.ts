af7ad7b78388a3ddb6d586f0b210be4f8dc299eb
zyad626 <52527072+zyad626@users.noreply.github.com>
2025-12-23 23:55:06 +0200

    remove unnecessary code

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -7,13 +7,4 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
-const connectDB = async () => {
-  try {
-    await prisma.$connect();
-  } catch (error) {
-    console.error(error);
-    await prisma.$disconnect();
-    process.exit(1);
-  }
-};
-export { prisma, connectDB };
+export { prisma };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import app from "./app";
 import config from "./config/config";
-import { connectDB } from "./config/prisma";
 
-connectDB().then(() => {
-  app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`);
-  });
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
 });
